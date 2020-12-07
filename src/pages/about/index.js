69d02e399d1c1c39c195c8754577fbc2c2166b66
Matthew <mdong@berkeley.edu>
2020-12-06 20:24:14 -0800

    add ewaste breakdown plot

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -50,16 +50,18 @@ export default class Index extends React.Component {
             <div className="content">
               <h1>Cal Recycle Data</h1>
               <p>
-                This visualization shows the amount of electronic waste produced per county in California based on data collected by CalRecycle.
-                <a href="/bibliography"> [4]</a>  Electric waste categories tracked by Cal Recycle include:
+                These visualizations show the amount of electronic waste produced per county in California and the breakdown of waste type for Alameda county based on data collected by CalRecycle.
+                <a href="/bibliography"> [4]</a>
               </p>
+              <iframe id="igraph" scrolling="no" seamless="seamless" src="https://chart-studio.plotly.com/~matthew-y-dong/202.embed?show_link=false" height="500px" width="100%"></iframe>
+              <iframe id="igraph" scrolling="no" seamless="seamless" src="https://chart-studio.plotly.com/~matthew-y-dong/206.embed?show_link=false" height="500px" width="100%"></iframe>
+              <p>Electric waste categories tracked by Cal Recycle include:</p>
               <ul>
                 <li>Brown Goods: generally larger, non-portable electronic goods that have some circuitry. Examples include microwaves, stereos, VCRs, DVD players, large radios, and audio/visual equipment. Does not include items with video display devices.</li>
                 <li>Video Display Devices: items with video displays larger than 4 inches that may or may not have a cathode ray tube (CRT), that are not included in the Other Small Consumer Electronics category. Includes televisions, computer monitors, and other items containing a cathode ray tube (CRT), portable DVD players, tablet computers (like the iPad and Kindle Fire), laptop computers, and non-CRT televisions (such as LCD televisions). The device is primarily intended to display moving video, perform computing functions, or view web content.</li>
                 <li>Computer-related Electronics: electronics with large circuitry that is computer-related, not including monitors. Examples include processors, keyboards, printers, fax machines, mice, disk drives, and modems.</li>
                 <li>Small Consumer Electronics: portable non-computer-related electronics with large circuitry. Examples include personal digital assistants (PDAs), cell phones (including those with a screen larger than 4 inches), phone systems, phone answering machines, portable electronic book readers (like Kindles and Nooks) and other devices for reading static text, computer games and other electronic toys, portable CD players, camcorders, digital cameras, cell phone chargers and other electronic device chargers, and other electronic devices.</li>
               </ul>
-              <iframe id="igraph" scrolling="no" seamless="seamless" src="https://chart-studio.plotly.com/~matthew-y-dong/202.embed?show_link=false" height="525" width="100%"></iframe>
             </div>
             </div>
         </section>
